27f9239a53a8c5720ed9df088bf607bba5f70f10
InvertedOwl <commandking123@gmail.com>
2026-02-01 10:50:32 -0700

    Allowed hosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["static.assemblock.dev", "assemblock.dev", "localhost"]
+  },
   build: {
     manifest: true,
     rollupOptions: {
