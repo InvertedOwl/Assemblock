756dc083921c41a41d21e99423987f9dd49d38f3
InvertedOwl <commandking123@gmail.com>
2026-02-01 13:28:01 -0700

    add trusted origins

--- _server/_server/settings.py
+++ _server/_server/settings.py
@@ -29,6 +29,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "assemblock.dev"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://assemblock.dev",
+    "https://www.assemblock.dev",
+]
+
 
 # Application definition
 
